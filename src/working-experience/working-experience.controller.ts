659e80bd07093ba15a6bd6bda090bd56f4b6e60a
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-04-01 16:25:46 +0200

    Fixing patch on working experience

--- src/working-experience/working-experience.controller.ts
+++ src/working-experience/working-experience.controller.ts
@@ -1,42 +1,66 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { WorkingExperienceService } from './working-experience.service';
 import { CreateWorkingExperienceInput } from './dto/create-working-experience.dto';
 import { UpdateWorkingExperienceInput } from './dto/update-working-experience.dto';
-import { Req, UseGuards } from '@nestjs/common/decorators';
+import { Patch, Req, UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { RequestWithUser } from 'src/common/interfaces/request-with-user.interface';
 
 @Controller('working-experience')
 export class WorkingExperienceController {
-  constructor(private readonly workingExperienceService: WorkingExperienceService) {}
+  constructor(
+    private readonly workingExperienceService: WorkingExperienceService,
+  ) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
-  create(@Req() request:RequestWithUser, @Body() createWorkingExperienceInput: CreateWorkingExperienceInput) {
-    return this.workingExperienceService.create(request.user, createWorkingExperienceInput);
+  create(
+    @Req() request: RequestWithUser,
+    @Body() createWorkingExperienceInput: CreateWorkingExperienceInput,
+  ) {
+    return this.workingExperienceService.create(
+      request.user,
+      createWorkingExperienceInput,
+    );
   }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
-  findAll(@Req() request:RequestWithUser,) {
+  findAll(@Req() request: RequestWithUser) {
     return this.workingExperienceService.findAll(request.user);
   }
 
   @Get(':id')
   @UseGuards(AuthGuard('jwt'))
-  findOne(@Req() request:RequestWithUser,@Param('id') id: string) {
-    return this.workingExperienceService.findOne(request.user,+id);
+  findOne(@Req() request: RequestWithUser, @Param('id') id: string) {
+    return this.workingExperienceService.findOne(request.user, +id);
   }
 
-  @Put(':id')
+  @Patch(':id')
   @UseGuards(AuthGuard('jwt'))
-  update(@Req() request:RequestWithUser, @Param('id') id: string, @Body() UpdateWorkingExperienceInput: UpdateWorkingExperienceInput) {
-    return this.workingExperienceService.update(request.user, +id, UpdateWorkingExperienceInput);
+  update(
+    @Req() request: RequestWithUser,
+    @Param('id') id: string,
+    @Body() UpdateWorkingExperienceInput: UpdateWorkingExperienceInput,
+  ) {
+    return this.workingExperienceService.update(
+      request.user,
+      +id,
+      UpdateWorkingExperienceInput,
+    );
   }
 
   @Delete(':id')
   @UseGuards(AuthGuard('jwt'))
-  remove(@Req() request:RequestWithUser,@Param('id') id: string) {
-    return this.workingExperienceService.remove(request.user,+id);
+  remove(@Req() request: RequestWithUser, @Param('id') id: string) {
+    return this.workingExperienceService.remove(request.user, +id);
   }
 }
